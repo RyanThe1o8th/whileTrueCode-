131dce5c2198b65736ebcc60507249f2f1cfa6d9
Kishi <kwijaya50@stuy.edu>
2025-05-08 11:01:21 -0400

    Updating HTML

--- app/__init__.py
+++ app/__init__.py
@@ -6,18 +6,24 @@ app.secret_key = 'whileTrueCode()-'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if 'username' not in session:
-        return redirect('/login')
-    return render_template('home.html')
+    return render_template('home.html', username = session.get('username'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     # If form info is from register, register the user; if info from login, user logs in
     if request.method == 'POST':
-        if request.form.get('register'):
-            return register_user()
         if request.form.get('login'):
             return login_user()
+        else:
+            flash('form error')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # If form info is from register, register the user; if info from login, user logs in
+    if request.method == 'POST':
+        if request.form.get('register'):
+            return register_user()
         else:
             flash('form error')
     return render_template('login.html')
