25d4b5efffaccafcc85b004ae0eefb1d9ddc71fa
Tanzeem Hasan <thasan50@stuy.edu>
2025-05-12 10:54:18 -0400

    finished all the templates time to link them together

--- app/__init__.py
+++ app/__init__.py
@@ -45,5 +45,69 @@ def profile():
 def testing():
     return render_template("testing.html", user=session.get('username'))
 
+// subway travel
+
+@app.route('/subway')
+def mainHome():
+    return render_template('home.html')
+
+@app.route('/neighborhood')
+def mainHome():
+    return render_template('neighborhood.html')
+
+@app.route('/mall')
+def mainHome():
+    return render_template('mall.html')
+
+@app.route('/school')
+def mainHome():
+    return render_template('school.html')
+
+// neighborhood
+
+@app.route('/home')
+def mainHome():
+    return render_template('mhome.html')
+
+@app.route('/friendHouse')
+def mainHome():
+    return render_template('friendHouse.html')
+
+@app.route('/park')
+def mainHome():
+    return render_template('park.html')
+
+// mall
+
+@app.route('/candy')
+def mainHome():
+    return render_template('candy.html')
+
+@app.route('/gamble')
+def mainHome():
+    return render_template('gamble.html')
+
+@app.route('/darkAlley')
+def mainHome():
+    return render_template('darkAlley.html')
+
+// school
+
+@app.route('/computerScienceLab')
+def mainHome():
+    return render_template('computerScienceLab.html')
+
+@app.route('/mathClassroom')
+def mainHome():
+    return render_template('mathClassroom.html')
+
+@app.route('/USHistory')
+def mainHome():
+    return render_template('USHistory.html')
+
+@app.route('/lunchroom')
+def mainHome():
+    return render_template('lunchroom.html')
+
 if __name__ == "__main__":
     app.run(host = '0.0.0.0')
