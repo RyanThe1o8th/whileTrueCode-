bc574772c34ea3354e06ca92de9925dc83c3144b
Ziyad Hamed <zydmain@gmail.com>
2025-05-13 10:24:33 -0400

    prelim num game logic

--- Games/guessTheNumber/game.py
+++ Games/guessTheNumber/game.py
@@ -1,8 +1,10 @@
-import random 
+import random
 
 def guess_the_number():
     number = random.randint(1, 100)
     attempts = 0
+
+    # Function to gen number, one takes number and return response, private variable
 
     print("Welcome to Guess the Number!")
     print("I'm thinking of a number between 1 and 100.")
@@ -22,6 +24,25 @@ def guess_the_number():
         else:
             print(f"Congratulations! You guessed the number in {attempts} attempts.")
             break
+
+def genNumber():
+    number = random.randint(1, 100)
+    return number
+
+def numberGame(number, attempts=0):
+    guess = int(input("Guess: "))
+    attempts += 1
+
+    if guess > number:
+        return "Too High!"
+    elif guess < number:
+        return "Too low!"
+    else:
+        return f"Congratulations! You guessed the number {number} in {attempts} attempts."
+
+    return numberGame(number, attempts)
+
+numberGame(genNumber())
 
 if __name__ == "__main__":
     guess_the_number()
